1f28d4ee29b9b15b537b7a5207a71f08282b523e
darkeox <darkeox@localhost>
2023-10-23 21:36:52 +0200

    enable pip rocm build

--- setup.py
+++ setup.py
@@ -4,26 +4,25 @@ import sys
 from pathlib import Path
 from setuptools import setup, find_packages
 
+try:
+    import torch
+    TORCH_AVAILABLE = True
+except ImportError:
+    TORCH_AVAILABLE = False
+
+IN_GITHUB_ACTIONS = os.environ.get("GITHUB_ACTIONS", "false") == "true"
+
 python_min_version = (3, 8, 0)
 python_min_version_str = '.'.join(map(str, python_min_version))
 if sys.version_info < python_min_version:
     print(f"You are using Python {platform.python_version()}. Python >={python_min_version_str} is required.")
     sys.exit(-1)
 
-BUILD_CUDA_EXT = int(os.environ.get('BUILD_CUDA_EXT', '1')) == 1
+CUDA_VERSION = "".join(os.environ.get("CUDA_VERSION", "").split("."))
 
-if BUILD_CUDA_EXT:
-    try:
-        import torch
-    except:
-        print("torch is not installed, please install torch first!")
-        sys.exit(-1)
-    CUDA_VERSION = "".join(torch.version.cuda.split("."))
-else:
-    CUDA_VERSION = "".join(os.environ.get("CUDA_VERSION", "").split("."))
-
+version = "0.3.2" + (f"+cu{CUDA_VERSION}" if CUDA_VERSION and IN_GITHUB_ACTIONS else "")
 common_setup_kwargs = {
-    "version": "0.3.2",
+    "version": version,
     "name": "auto_gptq",
     "author": "PanQiWei",
     "description": "An easy-to-use LLMs quantization package with user-friendly apis, based on GPTQ algorithm.",
@@ -46,17 +45,14 @@ common_setup_kwargs = {
     "python_requires": f">={python_min_version_str}"
 }
 
-if CUDA_VERSION:
-    common_setup_kwargs['version'] += f"+cu{CUDA_VERSION}"
-
 requirements = [
     "accelerate>=0.19.0",
     "datasets",
     "numpy",
     "rouge",
-    "torch>=1.13.0",
+    # "torch>=1.13.0",
     "safetensors",
-    "transformers>=4.31.0",
+    "transformers>=4.29.0",
     "peft"
 ]
 
@@ -66,40 +62,47 @@ extras_require = {
 
 include_dirs = ["autogptq_cuda"]
 
-additional_setup_kwargs = dict()
-if BUILD_CUDA_EXT:
-    from torch.utils import cpp_extension
-    from distutils.sysconfig import get_python_lib
-    conda_cuda_include_dir = os.path.join(get_python_lib(), "nvidia/cuda_runtime/include")
-    if os.path.isdir(conda_cuda_include_dir):
-        include_dirs.append(conda_cuda_include_dir)
-        print(f"appending conda cuda include dir {conda_cuda_include_dir}")
-    extensions = [
-        cpp_extension.CUDAExtension(
-            "autogptq_cuda_64",
-            [
-                "autogptq_cuda/autogptq_cuda_64.cpp",
-                "autogptq_cuda/autogptq_cuda_kernel_64.cu"
-            ]
-        ),
-        cpp_extension.CUDAExtension(
-            "autogptq_cuda_256",
-            [
-                "autogptq_cuda/autogptq_cuda_256.cpp",
-                "autogptq_cuda/autogptq_cuda_kernel_256.cu"
-            ]
-        )
-    ]
+if TORCH_AVAILABLE:
+    BUILD_CUDA_EXT = int(os.environ.get('BUILD_CUDA_EXT', '1')) == 1
+    
+    additional_setup_kwargs = dict()
+    if BUILD_CUDA_EXT and (torch.cuda.is_available() or IN_GITHUB_ACTIONS):
+        from torch.utils import cpp_extension
+        from distutils.sysconfig import get_python_lib
+        extensions = [
+            cpp_extension.CUDAExtension(
+                "autogptq_cuda_64",
+                [
+                    "autogptq_cuda/autogptq_hip_64.cpp",
+                    "autogptq_cuda/autogptq_hip_kernel_64.hip"
+                ]
+            ),
+            cpp_extension.CUDAExtension(
+                "autogptq_cuda_256",
+                [
+                    "autogptq_cuda/autogptq_hip_256.cpp",
+                    "autogptq_cuda/autogptq_hip_kernel_256.hip"
+                ]
+            )
+        ]
 
-    additional_setup_kwargs = {
-        "ext_modules": extensions,
-        "cmdclass": {'build_ext': cpp_extension.BuildExtension}
-    }
-common_setup_kwargs.update(additional_setup_kwargs)
-setup(
-    packages=find_packages(),
-    install_requires=requirements,
-    extras_require=extras_require,
-    include_dirs=include_dirs,
-    **common_setup_kwargs
-)
+        additional_setup_kwargs = {
+            "ext_modules": extensions,
+            "cmdclass": {'build_ext': cpp_extension.BuildExtension}
+        }
+    common_setup_kwargs.update(additional_setup_kwargs)
+    setup(
+        packages=find_packages(),
+        install_requires=requirements,
+        extras_require=extras_require,
+        include_dirs=include_dirs,
+        **common_setup_kwargs
+    )
+else:
+    setup(
+        packages=find_packages(),
+        install_requires=requirements,
+        extras_require=extras_require,
+        include_dirs=include_dirs,
+        **common_setup_kwargs
+    )
